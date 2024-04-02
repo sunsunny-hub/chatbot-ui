8d09bd624746bda71abffc4ec74c18121472a469
sunsunny-hub <surajsingh5233@gmail.com>
2024-04-02 11:11:47 +0530

    updated s3 bucket name

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "s3-tf-backend" # Replace with your actual S3 bucket name
+    bucket = "chatbot-tf-backend" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
